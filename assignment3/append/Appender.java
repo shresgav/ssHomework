20c5ddf746d1b6bb2e8e50179037fe4fe7e390e7
shresgav <shresgav@umich.edu>
2021-04-22 17:37:22 -0400

    assignment 3 almost completed

--- assignment3/append/Appender.java
+++ assignment3/append/Appender.java
@@ -1,10 +1,34 @@
 package assignment3.append;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+/**
+ * Appends user-submitted text to file path specified in the command line
+ * 
+ * @author Gavin Shrestha
+ *
+ */
+
 public class Appender {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.print("Text to be appended: ");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		sc.close();
+		appendText(s, args[0]);
 	}
-
+	
+	public static void appendText(String text, String path) {
+		try {
+			Files.write(Paths.get(path), text.getBytes(), StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
