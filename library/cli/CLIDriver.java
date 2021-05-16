6163a8985ca734a0831f6034dbb4916ca081fd6d
shresgav <shresgav@umich.edu>
2021-05-15 20:05:14 -0400

    something

--- library/cli/CLIDriver.java
+++ library/cli/CLIDriver.java
@@ -19,9 +19,6 @@ public class CLIDriver {
 	
 	public static void main(String[] args) {
 		while (true) {
-			for (int i = 0; i < 10; ++i) {
-				System.out.println("SAM IS GAY");
-			}
 			cli.mainMenu();
 			Integer option = readOption(sc);
 			while (option == -1) {
